33b91c313f10c6e8ce50fdbd66ce7f71b2087d42
Shohrat_Toyliyev <toyliyevshohrat@gmail.com>
2026-09-06 07:23:15 +0500

    fix: removed rotate device prompt

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import StartScreen from "./components/StartScreen";
-import RotateDevicePrompt from "./components/RotateDevicePrompt";
 import Game from "./game/Game";
 
 export default function App() {
@@ -8,7 +7,6 @@ export default function App() {
 
   return (
     <>
-      <RotateDevicePrompt />
       {screen === "menu" ? (
         <div className="flex flex-col justify-center items-center gap-4 bg-[url('/night-sky.webp')] bg-black bg-cover bg-no-repeat bg-center h-full font-mono text-white">
           <StartScreen onStart={() => setScreen("game")} />
